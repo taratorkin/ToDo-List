8d773a328fb21a55f640b50b46f888d2beb42f43
taratorkin <konstantintaratorkin@mail.ru>
2018-03-12 20:31:23 +0400

    user authentication with mongoose

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,7 @@ module.exports = app => {
 
   app.get('/google/callback', passport.authenticate('google'));
 
+  app.get('/api/currentUser', (req, res) => {
+    res.send(req.user);
+  });
 }
--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,42 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys.js');
-const mongoDb = require('../database/mongo.js');
+
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+  .then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/google/callback',
 }, (accessToken, refreshToken, profile, done) => {
-  mongoDb({
-    googleID: profile.id,
-    googleEmail: profile.emails[0].value
-  })
+      User.findOne({
+        googleID: profile.id
+      })
+      .then((existingUser) => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({
+            googleID: profile.id,
+            email: profile.emails[0].value
+          }).save().then((newUser) => {
+            done(null, newUser);
+          });
+        }
+      });
+
+
 }));
