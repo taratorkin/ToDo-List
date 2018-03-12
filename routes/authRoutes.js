ca87d10e388b08e3fa3b8f78c7f9c63a84aae059
taratorkin <konstantintaratorkin@mail.ru>
2018-03-12 22:28:53 +0400

    logout backend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,6 +8,11 @@ module.exports = app => {
 
   app.get('/google/callback', passport.authenticate('google'));
 
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
   app.get('/api/currentUser', (req, res) => {
     res.send(req.user);
   });
