aebaa497f9d7aeb43b52741253c65c2f4df07ff4
taratorkin <konstantintaratorkin@mail.ru>
2018-11-23 12:44:51 +0400

    v 0.5

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  googleClientID: PROCESS.ENV.GOOGLE_CLIENT_ID,
+  googleClientSecret: PROCESS.ENV.GOOGLE_CLIENT_SECRET,
+  mongoURI: PROCESS.ENV.MONGO_URI,
+  cookieKey: PROCESS.ENV.COOKIE_KEY
+}
